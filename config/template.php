38c16c260e56e2a8eef95c888b56835cc894d51e
Victor Danilo <victordanilo_cs@live.com>
2018-05-14 20:40:16 -0300

    include config file of template

---
+++ config/template.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+| Template configuration
+| -------------------------------------------------------------------
+| This file will contain the settings for the template library.
+|
+| 'parser' = if you want your views file to be parsed, set to TRUE
+| 'template' = template path
+ */
+
+$config['parser']   = false;
+$config['template'] = null;
